bbbe4af8e12ee473e95495e551dbe42aa8df27cd
VuTrinhNguyenHoang <trinhnguyenhoangvu306@gmail.com>
2025-09-14 01:38:54 +0700

    add some dependencies and arg for func get_cifar10

--- src/datasets/cifar10.py
+++ src/datasets/cifar10.py
@@ -2,16 +2,16 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-def get_cifar10(batch_size=128, num_workers=2):
+def get_cifar10(batch_size=128, root="./data", num_workers=2):
     transform = transforms.Compose([
         transforms.Resize(32),
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,))
     ])
 
-    trainset = torchvision.datasets.CIFAR10(root="./data", train=True,
+    trainset = torchvision.datasets.CIFAR10(root=root, train=True,
                                             download=True, transform=transform)
-    testset = torchvision.datasets.CIFAR10(root="./data", train=False,
+    testset = torchvision.datasets.CIFAR10(root=root, train=False,
                                            download=True, transform=transform)
 
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
